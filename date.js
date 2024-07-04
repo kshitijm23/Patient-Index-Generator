e8b34d4e9c31eb1b2d84ab275804fed0b8249c21
Kshitij Mahajan <89588959+kshitijm23@users.noreply.github.com>
2024-07-04 14:37:27 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,27 @@
+const dateInput = document.getElementById('date');
+
+// ✅ Using the visitor's timezone
+dateInput.value = formatDate();
+
+console.log(formatDate());
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function formatDate(date = new Date()) {
+  return [
+    date.getFullYear(),
+    padTo2Digits(date.getMonth() + 1),
+    padTo2Digits(date.getDate()),
+  ].join('-');
+}
+
+
+
+
+
+// ✅ Using UTC (universal coordinated time)
+// dateInput.value = new Date().toISOString().split('T')[0];
+
+// console.log(new Date().toISOString().split('T')[0]);
